c454ee9133744f020c104d3cfac3e502d89abccc
a-haru <47050280+a-haru@users.noreply.github.com>
2020-09-09 01:48:25 +0900

    画像の読み込み処理を修正

--- src/ImageContainer.ts
+++ src/ImageContainer.ts
@@ -11,11 +11,14 @@ class ImageContainer
     /**
      * 画像を保持する
      */
-    private images: Map<string, HTMLImageElement>;
+    private images: Map<string, HTMLImageElement> = new Map();
 
-    private constructor() {
-        this.images = new Map();
-    }
+    /**
+     * 画像読み込み用のPromiseを保持する
+     */
+    private promiseItems: Promise<boolean>[] = [];
+
+    private constructor() {}
 
     /**
      * インスタンスを返す
@@ -50,21 +53,35 @@ class ImageContainer
      */
     async load(path: string): Promise<boolean>
     {
-        return new Promise((resolve) => {
-            if (this.images.has(path)) {
-                // すでに読み込んでいたらtrueを返す
+        const promise = new Promise<boolean>((resolve) => {
+            // すでに読み込んでいたらtrueを返す
+            if (this.images.has(path)) return resolve(true);
+            
+            // 読み込まれていない画像なら読み込む
+            const img = new Image();
+            img.addEventListener('load', () => {
+                this.images.set(path, img);
                 resolve(true);
-            } else {
-                // 読み込まれていない画像なら読み込む
-                const img = new Image();
-                img.addEventListener('load', (e) => {
-                    this.images.set(path, img);
-                    resolve(true);
-                });
-                img.addEventListener('error', (e) => resolve(false));
-                img.src = path;
-            }
+            });
+            // 画像の読み込みに失敗したらfalseを返す
+            // エラーにしないためrejectはしない
+            img.addEventListener('error', () => {
+                resolve(false);
+            });
+            img.src = path;
         });
+
+        // 全て読み込み完了後に実行できるようにするため
+        this.promiseItems.push(promise);
+
+        return promise;
+    }
+
+    async isAllLoaded(): Promise<boolean>
+    {
+        return Promise.all(this.promiseItems)
+        .then( () => true)
+        .catch(() => false);
     }
 }
 
